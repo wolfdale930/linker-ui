f349ea5c5a975ae649acf951c93ba9c9a0d3a0de
wolfdale930 <wolfdale930@gmail.com>
2023-09-22 22:25:31 +0530

    updated input and button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'ui';
+import { ButtonModule, FormFieldModule, InputModule } from 'ui';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { ButtonModule } from 'ui';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    InputModule,
+    FormsModule,
+    FormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
